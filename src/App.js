1d5f115e8e4b66ad512498a8d909612bc8af85fe
anmolbansal7 <abanmolbansal5@gmail.com>
2021-08-03 20:36:08 +0530

    Lazy Scroll
    If you like it then keep it, otherwise we'll remove it

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import Solutions from './pages/Solutions';
 import Features from './pages/Features';
 import Contact from './pages/Contact';
 
+import SmoothScroll from "./components/SmoothScroll/SmoothScroll";
+
 function App(){
     return(
         <div>
-        <Navbar></Navbar>
-        <Landing></Landing>
-        <Mission></Mission>
-        <Challenges></Challenges>
-        <Solutions></Solutions>
-        <Features></Features>
-        <Contact></Contact>
+            <Navbar></Navbar>
+            <SmoothScroll>
+                <Landing></Landing>
+                <Mission></Mission>
+                <Challenges></Challenges>
+                <Solutions></Solutions>
+                <Features></Features>
+                <Contact></Contact>
+            </SmoothScroll>
         </div>
+        
+           
     )
 }
 
