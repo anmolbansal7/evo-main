2d54d134a60209450d5bbe8c6a689266ab8757b2
anmolbansal7 <abanmolbansal5@gmail.com>
2021-09-07 08:40:35 +0530

    v1.0.0

--- src/components/NavbarResp.js
+++ src/components/NavbarResp.js
@@ -2,14 +2,6 @@ import React from 'react'
 import './NavbarResp.css'
 
 function NavbarResp() {
-    function toggleChecked(){
-        document.getElementById('menu-btn').toggleAttribute('checked');
-        console.log("check")
-    }
-    var nk = document.getElementById('nk');
-    if(nk){
-        nk.addEventListener('click', toggleChecked);
-    }
     
     return (
         <div className="responsivenav">
@@ -18,8 +10,8 @@ function NavbarResp() {
                     <input class="menu-btn" type="checkbox" id="menu-btn" />
                     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
                     <ul class="menu">
-                        <li id="nk"><a href="#landing">Home</a></li>
-                        <li><a href="#mission" id="lnk">Mission</a></li>
+                        <li><a href="#landing"><input class="menu-btn" type="checkbox" id="menu-btn" /><label class="menu-icon" for="menu-btn">Home</label></a></li>
+                        <li><a href="#mission" id="lnk"><input class="menu-btn" type="checkbox" id="menu-btn" />Mission</a></li>
                         <li><a href="#challenges" id="lnk">Challenge</a></li>
                         <li><a href="#solutions" id="lnk">Solutions</a></li>
                         <li><a href="#features" id="lnk">Features</a></li>
