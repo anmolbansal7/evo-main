4502c170b2fac6d936c8ee877b7f481984912834
anmolbansal7 <abanmolbansal5@gmail.com>
2021-09-20 20:01:57 +0530

    bug fix

--- src/components/NavbarResp.js
+++ src/components/NavbarResp.js
@@ -1,26 +1,51 @@
-import React from 'react'
-import './NavbarResp.css'
+import React from "react";
+import "./NavbarResp.css";
 
 function NavbarResp() {
-    
-    return (
-        <div className="responsivenav">
-            <div class="menu-wrapper">
-                <header class="header">
-                    <input class="menu-btn" type="checkbox" id="menu-btn" />
-                    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-                    <ul class="menu">
-                        <li><a href="#landing"><input class="menu-btn" type="checkbox" id="menu-btn" /><label class="menu-icon" for="menu-btn">Home</label></a></li>
-                        <li><a href="#mission" id="lnk"><input class="menu-btn" type="checkbox" id="menu-btn" />Mission</a></li>
-                        <li><a href="#challenges" id="lnk">Challenge</a></li>
-                        <li><a href="#solutions" id="lnk">Solutions</a></li>
-                        <li><a href="#features" id="lnk">Features</a></li>
-                        <li><a href="#contact" id="lnk">Contact Us</a></li>
-                    </ul>
-                    </header>
-            </div>
-        </div>
-    )
+  return (
+    <div className="responsivenav">
+      <div class="menu-wrapper">
+        <header class="header">
+          <input class="menu-btn" type="checkbox" id="menu-btn" />
+          <label class="menu-icon" for="menu-btn">
+            <span class="navicon"></span>
+          </label>
+          <ul class="menu">
+            <li>
+              <a href="#landing" id="lnk">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#mission" id="lnk">
+                Mission
+              </a>
+            </li>
+            <li>
+              <a href="#challenges" id="lnk">
+                Challenge
+              </a>
+            </li>
+            <li>
+              <a href="#solutions" id="lnk">
+                Solutions
+              </a>
+            </li>
+            <li>
+              <a href="#features" id="lnk">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#contact" id="lnk">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </header>
+      </div>
+    </div>
+  );
 }
 
-export default NavbarResp
+export default NavbarResp;
